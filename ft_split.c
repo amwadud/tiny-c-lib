54c64931766d2658c519d01119a8bffc9cd21d67
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-19 15:54:51 +0100

    Auto update on Sun Oct 19 03:54:51 PM +01 2025

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,66 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:20:24 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/18 14:35:48 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/19 07:08:20 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// TODO: Build this.
-
-/* char	**ft_split(char const *s, char c)
+static size_t	ft_countword(const char *s, char c)
 {
-	return (NULL);
-} */
+	size_t	i;
+	size_t	count;
+
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		if (s[i] != c && s[i])
+			count++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	n;
+	size_t	count;
+	char	**res;
+	
+	res = (char **)malloc(sizeof(char *) * (ft_countword(s, c) + 1));
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		j = 0;
+		while (s[i + j] != c && s[i + j])
+			j++;
+		if (j > 0)
+		{
+			res[count] = (char *)malloc(j + 1); 
+			ft_memcpy(res[count], s + i, j);
+			count++;
+		}
+		i += j;
+	}
+	return (res);
+}
+
+int	main(void)
+{
+	char	**s = ft_split("  Hello world  ", ' ');
+	size_t	i;
+	#include <stdio.h>
+	i = 0;
+	while (s[i] != NULL)
+	{
+		printf("%s\n", s[i]);
+		i++;
+	}
+}
