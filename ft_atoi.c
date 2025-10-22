cdacd2375158327970efc149a531ba6a346975b7
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-22 10:51:17 +0100

    Doing some crazy stuff with ft_atoi :)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:23:46 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/19 22:57:33 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/22 10:09:46 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,24 @@ int	ft_atoi(const char *str)
 	i = 0;
 	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
-	sign = 1;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
+	sign = 1 - 2 * (str[i] == '-');
 	res = 0;
-	while (ft_isdigit(str[i]))
+	/* if (str[i] == '-' || str[i] == '+')
+		i++; */
+ 	while (ft_isdigit(str[i]) || str[i] == '+' || str[i] == '-')
 	{
-		res = (res * 10) + (*(str + i) - '0');
+		res = ((res * 10) + (*(str + i) - '0')) * ft_isdigit(str[i]);
 		i++;
+		// i++;
 	}
 	return (res * sign);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%i\n", ft_atoi("-51"));
+	printf("%i\n", ft_atoi("+51"));
+	printf("%i\n", ft_atoi("51"));
 }
