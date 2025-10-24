c5a17d70929590f3146d392a005df549b3a7e816
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-25 00:37:28 +0100

    Add testing.

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:20:32 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/14 22:20:32 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/25 00:32:05 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 }
+
+/*
+#include <unistd.h>
+
+int	main(void)
+{
+	ft_putstr_fd("Hello World", STDOUT_FILENO);
+} */
