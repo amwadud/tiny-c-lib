97a979d683f248e0e327b6d0924ac238c66580e3
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-25 00:32:19 +0100

    Add test case.

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:20:53 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/14 22:20:53 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/25 00:31:16 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,14 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+
+/*
+#include "libft.h"
+
+int main(void)
+{
+    ft_putchar_fd('A', 1);
+    ft_putchar_fd('\n', 1);
+    return 0;
+}
+*/
