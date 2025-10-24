f0355ee61aba532770012c584042b0dde86ff7f9
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-25 00:24:25 +0100

    Optimize.

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,27 +6,43 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:20:38 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/22 12:07:55 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/25 00:10:19 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
- * FIXME: Gha katkhawar hna.
- */
+
+# define FT_INT_MIN     (-2147483647 - 1)
+# define FT_INT_MIN_STR "-2147483648"
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == (-2147483648))
+	if (n == FT_INT_MIN)
 	{
-		write(fd, "-2147483648", ft_strlen("-2147483648"));
+		ft_putstr_fd(FT_INT_MIN_STR, fd);
 		return ;
 	}
 	if (n < 0)
 	{
-		n *= -1;
 		ft_putchar_fd('-', fd);
+		n = -n;
 	}
 	if (n > 9)
 		ft_putnbr_fd(n / 10, fd);
 	ft_putchar_fd((n % 10) + '0', fd);
 }
+
+/*
+int main(void)
+{
+    int test_numbers[] = {0, 42, -42, 2147483647, -2147483648};
+    size_t i;
+
+    for (i = 0; i < sizeof(test_numbers)/sizeof(test_numbers[0]); i++)
+    {
+        ft_putnbr_fd(test_numbers[i], 1);  // 1 = stdout
+        ft_putchar_fd('\n', 1);            // print newline
+    }
+    return 0;
+}
+*/
