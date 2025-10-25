a122c6eba58ada04535a8d74f5810a8350bebbcf
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-25 01:42:09 +0100

    Optimize and add tests.

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,11 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:21:31 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/14 22:21:31 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/25 01:38:02 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// WARN: Make sure about the casting for c to unsigned char.
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -22,10 +20,33 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (*((unsigned char *)s + i) == (unsigned char)c)
-		{
-			return ((void *)(s + i));
-		}
+			return ((void *)((unsigned char *)s + i));
 		i++;
 	}
 	return (NULL);
 }
+
+/*
+#include <assert.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[] = "Hello, World!";
+    char *p;
+
+    // find 'W'
+    p = ft_memchr(str, 'W', strlen(str));
+    assert(p == str + 7);
+
+    // find 'z' (not found)
+    p = ft_memchr(str, 'z', strlen(str));
+    assert(p == NULL);
+
+    // n = 0
+    p = ft_memchr(str, 'H', 0);
+    assert(p == NULL);
+
+    return 0;
+}
+*/
