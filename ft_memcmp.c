bd4781f78abcfd60e4a7f2312cfd7aa8f6465633
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-25 01:37:15 +0100

    Add tests and optimize.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:21:24 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/18 17:18:22 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/25 01:23:04 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,34 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < (n - 1))
+	while (i < n)
 	{
-		if ((*(unsigned char *)s1 + i) != *((unsigned char *)s2 + i))
-			break ;
+		if (*((unsigned char *)s1 + i) != *((unsigned char *)s2 + i))
+			return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
 		i++;
 	}
-	return ((*(unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
+	return 0;
 }
+
+/*
+#include "libft.h"
+#include <assert.h>
+
+int main(void)
+{
+    char a[] = "Hello";
+    char b[] = "Hello";
+    char c[] = "Helxo";
+
+    // identical strings
+    assert(ft_memcmp(a, b, 5) == 0);
+
+    // different strings
+    assert(ft_memcmp(a, c, 5) < 0); // 'l' - 'x' < 0
+
+    // partial comparison
+    assert(ft_memcmp(a, c, 3) == 0); // first 3 bytes equal
+
+    return 0;
+}
+*/
